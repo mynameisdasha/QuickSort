f40c577716b8d890becd129dbb2b9f28942b63c4
mynameisdasha <128239152+mynameisdasha@users.noreply.github.com>
2023-03-18 18:06:20 +0300

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,22 @@
+a=[int(i) for i in input().split()]
+def quickSort(a):
+    if (len(a)<2):
+        return a
+    if (len(a)==2):
+        if (a[0]<a[1]):
+            return a[0],a[1]
+        else:
+            return a[1],a[0]
+    b=len(a)//2
+    j=0 
+    k=len(a)-1
+    while a[j]<a[b]:
+        j+=1
+    while a[k]>a[b]:
+        k-=1
+    w=a[j]
+    a[j]=a[k]
+    a[k]=w
+    return list(quickSort([x for x in a if x<a[b]]))+list([x for x in a if x==a[b]])+list(quickSort([x for x in a if x>a[b]]))
+O=quickSort(a)
+print(O)
